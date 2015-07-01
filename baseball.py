142eabae723d73c8c90b8f01645ffd2844f20608
Administrator <Administrator@WIN-NSMJ77VUG4E>
2015-07-01 21:10:56 +0900

    [150701] except kimjongho

--- baseball.py
+++ baseball.py
@@ -10,61 +10,7 @@ def basecraw(url):
 	soup = BeautifulSoup(page)
 	return soup
 
-player_all=basecraw("http://www.koreabaseball.com/Record/Player/HitterDetail/Game.aspx?playerId=77454")
-# print nckjh_all
-player_select = player_all.select(".player_records > table")
-head = player_select[0].select("thead")
-#not ready table
-mid=player_select[0].select("tbody")
-#bring player name
-playername = player_all.select("#cphContainer_cphContents_playerProfile_lblName")
- 
-#make header
-header=["구분"]
-for i in range(len(head[0].select("a"))):
-		header.append(head[0].select("a")[i]['title'])
-
-#make table
-# for i in range(len(header)):
-# 	print header[i],		
-# for i in range(len(mid[0].select("td"))):
-# 	print mid[0].select("td")[i].string,
-# 	if (i+1)%16 == 0:
-# 		print "\n"
-
-# print mid[0].select("td")[1].string
-# vsteam_record=mid[0].td.string
-# print vsteam_record	
-
- 
-import xlwt
-book = xlwt.Workbook(encoding="utf-8")
- #make sheet
-sheet1 = book.add_sheet("Sheet 1")
-#make content
-row=0
-sheet1.write(row, 16, "선수명") 
-for i in range(len(header)):
-      
-    sheet1.write(row, i, header[i])
-    if (i+1)%16 == 0:
-        row=row+1
-   
-col=0
-for i in range(len(mid[0].select("td"))):
-    sheet1.write(row, col, mid[0].select("td")[i].string)
-    
-    col=col+1
-    if (i+1)%16 == 0:
-        sheet1.write(row, 16, playername[0].string)
-        row=row+1
-        col=0
-     
-
- # the other player
- 
- 
-for k in range(75000,79999):
+for k in range(75124,75126):
     player_all=basecraw("http://www.koreabaseball.com/Record/Player/HitterDetail/Game.aspx?playerId="+str(k))
     # print nckjh_all
     player_select = player_all.select(".player_records > table")
@@ -76,6 +22,28 @@ for k in range(75000,79999):
     beforeteam = player_all.select("#cphContainer_cphContents_playerProfile_lblCareer")
     nowteam = player_all.select(".player_info > .team")
     
+    
+    #bring header
+    
+    head = player_select[0].select("thead")
+    #make header
+    header=["구분"]
+    for i in range(len(head[0].select("a"))):
+            header.append(head[0].select("a")[i]['title'])
+    import xlwt
+    book = xlwt.Workbook(encoding="utf-8")
+     #make sheet
+    sheet1 = book.add_sheet("Sheet 1")
+    #make content
+    row=0
+    sheet1.write(row, 16, "선수명") 
+    for i in range(len(header)):
+          
+        sheet1.write(row, i, header[i])
+        
+        
+    if (i+1)%16 == 0:
+        row=row+1
      
     y="LG".decode('utf-8')
     x = "데이터가 존재하지 않습니다.".decode('utf-8')
